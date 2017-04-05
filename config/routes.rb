19d1b24bb9621b0bf16430134f9102a815d57073
Pongpol Hussein <pongpol.bruno@gmail.com>
2017-04-05 11:38:50 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-
-  devise_for :users, :only => [:sessions, :registrations]
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   root to: 'application#index'
   get '/exam', to: 'exam#index'
   get '/exam/:n', to: 'exam#index', as: :examwithlevel
-
   namespace :admin do
     get '/', to: 'admin#index'
-    resources :question, :only => [:index, :new, :create, :edit, :update]
+    resources :question, only: [:index, :new, :create, :edit, :update]
     post '/questiondelete/:id', to: 'question#destroy', as: :question_delete
   end
-
 end
